6f7e3f1c036775473fbb4547091e4f278fec7cdf
shinshou <mg0x42y7214f7c@gmail.com>
2023-08-16 16:00:06 +0900

    sqlite config delete

--- flask_chat_server/__init__.py
+++ flask_chat_server/__init__.py
@@ -70,11 +70,11 @@ from sqlalchemy.engine import Engine
 from sqlalchemy import event
 
 
-@event.listens_for(Engine, "connect")
-def set_sqlite_pragma(dbapi_connection, connection_record):
-    cursor = dbapi_connection.cursor()
-    cursor.execute("PRAGMA foreign_keys=ON")
-    cursor.close()
+# @event.listens_for(Engine, "connect")
+# def set_sqlite_pragma(dbapi_connection, connection_record):
+#     cursor = dbapi_connection.cursor()
+#     cursor.execute("PRAGMA foreign_keys=ON")
+#     cursor.close()
 
 
 # from flask_chat_server.users.views import users
